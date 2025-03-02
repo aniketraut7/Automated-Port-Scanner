0c765064bdee9691ff870171b1375771ea48a54d
aniketraut7 <87106536+aniketraut7@users.noreply.github.com>
2025-03-02 10:55:14 +0530

    setup.sh

--- setup.sh
+++ setup.sh
@@ -2,30 +2,26 @@
 
 echo "🚀 Setting up Auto_Scanner on Kali Linux..."
 
-# Step 1: Update the system
-echo "[+] Updating system packages..."
-sudo apt update -y && sudo apt upgrade -y
-
-# Step 2: Install required dependencies
+# Step 1: Install required dependencies
 echo "[+] Installing Python and required dependencies..."
 sudo apt install python3 python3-pip python3-venv nmap -y
 
-# Step 3: Create a virtual environment
+# Step 2: Create a virtual environment
 echo "[+] Creating a virtual environment..."
 python3 -m venv venv
 
-# Step 4: Activate the virtual environment
+# Step 3: Activate the virtual environment
 echo "[+] Activating the virtual environment..."
 source venv/bin/activate
 
 pip install -r requirements.txt
 
-# Step 5: Install Python dependencies
+# Step 4: Install Python dependencies
 echo "[+] Installing necessary Python libraries..."
 pip install --upgrade pip
 pip install -r requirements.txt
 
-# Step 6: Verify Installation
+# Step 5: Verify Installation
 echo "[+] Verifying installation..."
 python3 -m pip list
 
